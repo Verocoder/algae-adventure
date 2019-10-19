d097a7ce374eb6ac60eaad751e0fb31e9317fc7c
verocoder <david@verophotography.co.uk>
2019-10-19 17:20:33 +0100

    fixed

--- gameplay.js
+++ gameplay.js
@@ -9,78 +9,78 @@ const gameplay = {
       algal_ceiling: 10000,
       growth_factor: 4,
       resource_availability: 5
-    },
-    climate: {
-      title: "Cimate",
-      eligible_areas: ["river", "sea"],
+    }
+  },
+  climate: {
+    title: "Cimate",
+    eligible_areas: ["river", "sea"],
+    description: "",
+    helpText: "",
+    options: [
+      {
+        title: "warm",
+        description:
+          "THis is a warm climate. as a bloom ou will do super well here",
+        effect: {
+          algae_survive: 0.8,
+          growth_factor: 1,
+          long_term_growth_factor: 5
+        }
+      },
+      {
+        title: "cold",
+        description:
+          "THis is a warm climate. as a bloom ou will do super well here",
+        effect: {
+          algae_survive: 0.8,
+          growth_factor: 1,
+          long_term_growth_factor: 0.5
+        }
+      }
+    ]
+  },
+  choices: [
+    {
+      title: "Some birds nest",
+      eligible_areas: ["river"],
       description: "",
       helpText: "",
       options: [
         {
-          title: "warm",
-          description:
-            "THis is a warm climate. as a bloom ou will do super well here",
+          title: "shoe them away",
+          description: "",
           effect: {
-            algae_survive: 0.8,
+            algae_survive: 1,
             growth_factor: 1,
-            long_term_growth_factor: 5
+            long_term_growth_factor: 1
           }
         },
         {
-          title: "cold",
-          description:
-            "THis is a warm climate. as a bloom ou will do super well here",
+          title: "Let them stay",
+          description: "",
           effect: {
             algae_survive: 0.8,
-            growth_factor: 1,
-            long_term_growth_factor: 0.5
+            growth_factor: 5,
+            long_term_growth_factor: 1
           }
         }
       ]
-    },
-    choices: [
-      {
-        title: "Some birds nest",
-        eligible_areas: ["river"],
-        description: "",
-        helpText: "",
-        options: [
-          {
-            title: "shoe them away",
-            description: "",
-            effect: {
-              algae_survive: 1,
-              growth_factor: 1,
-              long_term_growth_factor: 1
-            }
-          },
-          {
-            title: "Let them stay",
-            description: "",
-            effect: {
-              algae_survive: 0.8,
-              growth_factor: 5,
-              long_term_growth_factor: 1
-            }
-          }
-        ]
-      }
-    ],
+    }
+  ],
 
-    random_events: [
-      {
-        title: "there was a warm spell",
-        eligible_areas: ["river", "sea"],
-        description: "",
-        helpText: "",
-        effect: {
-          algae_survive: 0.8,
-          growth_factor: 1,
-          long_term_growth_factor: 1
-        }
+  random_events: [
+    {
+      title: "there was a warm spell",
+      eligible_areas: ["river", "sea"],
+      description: "",
+      helpText: "",
+      effect: {
+        algae_survive: 0.8,
+        growth_factor: 1,
+        long_term_growth_factor: 1
       }
-    ]
-  }
+    }
+  ]
 };
 
 module.exports = gameplay;
