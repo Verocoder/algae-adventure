e2691fbc804587129556b00be5e28485bfd0032a
Darrian <rikkuness@gmail.com>
2019-10-19 16:00:08 +0100

    padding change

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Container>
 
-      <Button.Group size='huge' fluid style={{ padding: '10px 0px' }}>
+      <Button.Group size='huge' fluid style={{ padding: '30px 0px' }}>
         <Button>A</Button>
         <Button.Or />
         <Button>B</Button>
