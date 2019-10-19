86f4956ec93fadce51b7e97da509c772ed47f7a4
Darrian <rikkuness@gmail.com>
2019-10-19 15:58:40 +0100

    set state value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import { Button, Sidebar, Statistic, Container, Divider } from 'semantic-ui-react'
 
 export default () => {
+  const [value, setValue] = useState(1234)
+
   return <div style={{ width: '100vw', height: '100vh' }}>
     <Sidebar direction='right' visible >
       <Statistic.Group widths='1' style={{ paddingTop: 20 }}>
         <Statistic>
-          <Statistic.Value>1,234</Statistic.Value>
-          <Statistic.Label>Algae per second</Statistic.Label>
+          <Statistic.Value>{value}</Statistic.Value>
+          <Statistic.Label>km2</Statistic.Label>
         </Statistic>
       </Statistic.Group>
     </Sidebar>
@@ -21,7 +23,7 @@ export default () => {
       <Divider />
 
       <Container>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Container>
 
       <Button.Group size='huge' fluid style={{ padding: '10px 0px' }}>
