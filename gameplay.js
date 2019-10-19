9e977eccabc1a76889b147212170838a1da642a8
verocoder <david@verophotography.co.uk>
2019-10-19 17:19:22 +0100

    formatted

--- gameplay.js
+++ gameplay.js
@@ -1,60 +1,86 @@
 const gameplay = {
-  locations:{
-    shallow_sea:{
-      "title": "Shallow Sea",
-      "description": "A shallow sea area near the coast",
-      "helpText": "Shallow sea areas have some kind of feature that make them either good or not good for growing algae", 
-      "image":"location of image",
-      "algal_ceiling":10000,
-      "growth_factor":4, 
-      "resource_availability":5
-  },
-  climate:{
-    "title": "Cimate",
-    eligible_areas:["river","sea"],
-    "description": "",
-    "helpText": "",
-    options:[
-    {
-      title:"warm",
-      description: "THis is a warm climate. as a bloom ou will do super well here",
-      effect:{"algae_survive":0.8, "growth_factor": 1, "long_term_growth_factor":5}
-    },{
-      title:"cold",
-      description: "THis is a warm climate. as a bloom ou will do super well here",
-      effect:{"algae_survive":0.8, "growth_factor": 1, "long_term_growth_factor":0.5}
-    }
-  ]},
-  choices: [
-    {
-      "title": "Some birds nest",
-      eligible_areas:["river"],
-      "description": "",
-      "helpText": "",
-      options:[
+  locations: {
+    shallow_sea: {
+      title: "Shallow Sea",
+      description: "A shallow sea area near the coast",
+      helpText:
+        "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
+      image: "location of image",
+      algal_ceiling: 10000,
+      growth_factor: 4,
+      resource_availability: 5
+    },
+    climate: {
+      title: "Cimate",
+      eligible_areas: ["river", "sea"],
+      description: "",
+      helpText: "",
+      options: [
+        {
+          title: "warm",
+          description:
+            "THis is a warm climate. as a bloom ou will do super well here",
+          effect: {
+            algae_survive: 0.8,
+            growth_factor: 1,
+            long_term_growth_factor: 5
+          }
+        },
+        {
+          title: "cold",
+          description:
+            "THis is a warm climate. as a bloom ou will do super well here",
+          effect: {
+            algae_survive: 0.8,
+            growth_factor: 1,
+            long_term_growth_factor: 0.5
+          }
+        }
+      ]
+    },
+    choices: [
       {
-        title: "shoe them away",
+        title: "Some birds nest",
+        eligible_areas: ["river"],
         description: "",
-        effect:{"algae_survive":1, "growth_factor": 1, "long_term_growth_factor":1}
-      },{
-        title: "Let them stay",
-        description: "",
-        effect:{"algae_survive":0.8, "growth_factor": 5, "long_term_growth_factor":1}
+        helpText: "",
+        options: [
+          {
+            title: "shoe them away",
+            description: "",
+            effect: {
+              algae_survive: 1,
+              growth_factor: 1,
+              long_term_growth_factor: 1
+            }
+          },
+          {
+            title: "Let them stay",
+            description: "",
+            effect: {
+              algae_survive: 0.8,
+              growth_factor: 5,
+              long_term_growth_factor: 1
+            }
+          }
+        ]
       }
-    ]
-    }
-  ] ,
+    ],
 
-  random_events:[
+    random_events: [
       {
-      "title": "there was a warm spell",
-      eligible_areas:["river","sea"],
-      "description": "",
-      "helpText": "",
-      effect:{"algae_survive":0.8, "growth_factor": 1, "long_term_growth_factor":1}
+        title: "there was a warm spell",
+        eligible_areas: ["river", "sea"],
+        description: "",
+        helpText: "",
+        effect: {
+          algae_survive: 0.8,
+          growth_factor: 1,
+          long_term_growth_factor: 1
+        }
       }
     ]
-  }       
+  }
 };
 
 module.exports = gameplay;
