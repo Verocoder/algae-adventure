0fbb7a436599da77f773dc6a335073d3d643b4aa
Darrian <rikkuness@gmail.com>
2019-10-19 15:54:18 +0100

    fix padding

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default () => {
       </Statistic.Group>
     </Sidebar>
 
-    <div style={{ marginRight: 260, padding: 10 }}>
+    <div style={{ marginRight: 260, padding: 20 }}>
       <video controls style={{ width: '100%' }}>
         <source src='http://techslides.com/demos/sample-videos/small.mp4' type='video/mp4' />
       </video>
@@ -24,7 +24,7 @@ export default () => {
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Container>
 
-      <Button.Group size='huge' fluid style={{ padding: '10px 30px' }}>
+      <Button.Group size='huge' fluid style={{ padding: '10px 0px' }}>
         <Button>A</Button>
         <Button.Or />
         <Button>B</Button>
