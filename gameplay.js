3e53d8e87a62435ce658b102d925cfe74cc351ef
stevenjfr <studio@jamman.co.uk>
2019-10-19 15:17:30 +0100

    Add react app and JS game data

---
+++ gameplay.js
@@ -0,0 +1,20 @@
+const gameplay = {
+  warmClimate: {
+    title: "Warm climate",
+    videoLocation: "drive://warm.mp4",
+    points: 10,
+    leftButtonKey: "",
+    rightButtonKey: "",
+    HelpText: "OMG this is a warm climate"
+  },
+  coldClimate: {
+    title: "Cold climate",
+    videoLocation: "drive://cold.mp4",
+    points: -5,
+    leftButtonKey: "",
+    rightButtonKey: "",
+    HelpText: "Oh no this is a cold climate"
+  }
+};
+
+module.exports = gameplay;
