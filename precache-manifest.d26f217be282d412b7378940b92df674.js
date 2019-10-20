9383600f639fdc766899ba032fbe4b5dfc7401d9
verocoder <david@verophotography.co.uk>
2019-10-20 14:16:11 +0100

    Updates

---
+++ precache-manifest.d26f217be282d412b7378940b92df674.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d302e0ef1c9c8cca7e80fc69d6b65d0a",
+    "url": "/algae-adventure/index.html"
+  },
+  {
+    "revision": "9a5d37fddb2e1acc93dd",
+    "url": "/algae-adventure/static/css/2.18dba977.chunk.css"
+  },
+  {
+    "revision": "1430b0a0165ea61b2d44",
+    "url": "/algae-adventure/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9a5d37fddb2e1acc93dd",
+    "url": "/algae-adventure/static/js/2.65608a82.chunk.js"
+  },
+  {
+    "revision": "1430b0a0165ea61b2d44",
+    "url": "/algae-adventure/static/js/main.5055ae7c.chunk.js"
+  },
+  {
+    "revision": "04d7b997e30a44545770",
+    "url": "/algae-adventure/static/js/runtime-main.a5ec1a3c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/algae-adventure/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/algae-adventure/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/algae-adventure/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/algae-adventure/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/algae-adventure/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/algae-adventure/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/algae-adventure/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/algae-adventure/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/algae-adventure/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/algae-adventure/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/algae-adventure/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/algae-adventure/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/algae-adventure/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/algae-adventure/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/algae-adventure/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/algae-adventure/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
