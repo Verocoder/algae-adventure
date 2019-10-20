cae179b1898f0adeb30aa90f73092e10ffb06264
Savannah Storm <savannahstorm@Savannahs-MacBook-Pro.local>
2019-10-20 09:01:09 +0100

    temp

--- gameplay.js
+++ gameplay.js
@@ -1,7 +1,7 @@
 const gameplay = {
   warmClimate: {
     title: "Warm climate",
-    description: "THis is a warm climate. as a bloom ou will do super well here"
+    description: "THis is a warm climate. as a bloom ou will do super well here",
     videoLocation: "drive://warm.mp4",
     points: 10,
     leftButtonKey: "",
@@ -11,6 +11,16 @@ const gameplay = {
     HelpText: "OMG this is a warm climate"
   },
   coldClimate: {
+    title: "Cold climate",
+    videoLocation: "drive://cold.mp4",
+    points: -5,
+    leftButtonKey: "",
+    rightButtonKey: "",
+    leftButtonChoiceText: "",
+    rightButtonChoiceText: "highlyPopulated",
+    HelpText: "Oh no this is a cold climate"
+  },
+  highlyPopulated: {
     title: "Cold climate",
     videoLocation: "drive://cold.mp4",
     points: -5,
