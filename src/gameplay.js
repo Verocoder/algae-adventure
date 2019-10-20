4cd3bd0188826b96c2aeaaee36de5663210f04bf
verocoder <david@verophotography.co.uk>
2019-10-20 09:40:56 +0100

    added options info to events

--- src/gameplay.js
+++ src/gameplay.js
@@ -3,7 +3,8 @@ const gameplay = {
     shallow_sea: {
       title: "Shallow Sea",
       description: "A shallow sea area near the coast",
-      image: "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+      image:
+        "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
       helpText:
         "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
       algal_ceiling: 1000000000000,
@@ -13,7 +14,8 @@ const gameplay = {
       title: "Deep Sea",
       description: "A shallow sea area near the coast",
       video: null,
-      image: "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+      image:
+        "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
       helpText:
         "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
       algal_ceiling: 1000000000000000,
@@ -23,7 +25,8 @@ const gameplay = {
       title: "Lowland River",
       description: "A shallow sea area near the coast",
       video: null,
-      image: "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+      image:
+        "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
       helpText:
         "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
       algal_ceiling: 1000000000,
@@ -33,7 +36,8 @@ const gameplay = {
       title: "Lowland River",
       description: "A shallow sea area near the coast",
       video: null,
-      image: "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+      image:
+        "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
       helpText:
         "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
       algal_ceiling: 1000000000,
@@ -43,7 +47,8 @@ const gameplay = {
       title: "Lakes and Ponds",
       description: "A shallow sea area near the coast",
       video: null,
-      image: "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+      image:
+        "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
       helpText:
         "Shallow sea areas have some kind of feature that make them either good or not good for growing algae",
       algal_ceiling: 1000000,
@@ -53,7 +58,8 @@ const gameplay = {
   climate: {
     title: "Cimate",
     video: null,
-    image:"https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+    image:
+      "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
     // all areas
     eligible_areas: ["river", "shallow_sea"],
     description: "",
@@ -62,11 +68,11 @@ const gameplay = {
       {
         title: "warm",
         video: null,
-        image:"",
+        image: "",
         description:
           "THis is a warm climate. as a bloom ou will do super well here",
         effect: {
-          duration:-1,
+          duration: -1,
           algae_survive: 1,
           growth_factor: 5
         }
@@ -74,11 +80,12 @@ const gameplay = {
       {
         title: "cold",
         video: null,
-        image:"https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
+        image:
+          "https://cff2.earth.com/uploads/2019/07/26121452/Algae-may-have-joined-with-fungi-to-give-rise-to-the-first-terrestrial-plants-730x410.jpg",
         description:
           "THis is a warm climate. as a bloom ou will do super well here",
         effect: {
-          duration:-1,
+          duration: -1,
           algae_survive: 1,
           growth_factor: 0.5
         }
@@ -89,7 +96,7 @@ const gameplay = {
     {
       title: "Some birds nest",
       video: null,
-      image:"",
+      image: "",
       eligible_areas: ["river", "lake"],
       description: "",
       helpText: "",
@@ -97,10 +104,10 @@ const gameplay = {
         {
           title: "shoe them away",
           video: null,
-          image:"",
+          image: "",
           description: "",
           effect: {
-            duration:0,
+            duration: 0,
             algae_survive: 1,
             growth_factor: 1
           }
@@ -108,70 +115,88 @@ const gameplay = {
         {
           title: "Let them stay",
           video: null,
-          image:"",
+          image: "",
           description: "",
           effect: {
-            duration:4,
+            duration: 4,
             algae_survive: 1,
             growth_factor: 5
           }
         }
       ]
-    },{
+    },
+    {
       title: "Sewage",
-    video: null,
-    image:"",
-    eligible_areas: ["river", "lake"],
-    description: "A local sewage company want to use your lake for drainage, for a handsome price. ",
-    helpText: "",
-    options: [
-      {
-        title: "Let Them",
-        video: null,
-        image:"",
-        description: "",
-        effect: {
-          duration:28,
-          algae_survive: 1,
-          growth_factor: 10
+      video: null,
+      image: "",
+      eligible_areas: ["river", "lake"],
+      description:
+        "A local sewage company want to use your lake for drainage, for a handsome price. ",
+      helpText: "",
+      options: [
+        {
+          title: "Let Them",
+          video: null,
+          image: "",
+          description: "",
+          effect: {
+            duration: 28,
+            algae_survive: 1,
+            growth_factor: 10
+          }
+        },
+        {
+          title: "Say No",
+          video: null,
+          image: "",
+          description: "",
+          effect: {
+            duration: 0,
+            algae_survive: 1,
+            growth_factor: 1
+          }
         }
-      },
-      {
-        title: "Say No",
-        video: null,
-        image:"",
-        description: "",
-        effect: {
-          duration:0,
-          algae_survive: 1,
-          growth_factor: 1
-        }
-      }
-    ]
-  },{
+      ]
+    },
+    {
       title: "There was a warm spell",
       eligible_areas: ["river", "lake", "shallow_sea", "deep_sea"],
       description: "",
       helpText: "",
-      options: [ {
-        duration:2,
-        algae_survive: 1,
-        growth_factor: 2
-      }]
-    },{
+      options: [
+        {
+          title: "shoe them away",
+          video: null,
+          image: "",
+          description: "",
+          effect: {
+            duration: 2,
+            algae_survive: 1,
+            growth_factor: 2
+          }
+        }
+      ]
+    },
+    {
       title: "there was a big storm",
       eligible_areas: ["river", "lake", "shallow_sea", "deep_sea"],
       description: "",
       helpText: "",
-      options: [{
-        duration:1,
-        algae_survive: 0.6,
-        growth_factor: 0.5
-      }]
+      options: [
+        {
+          title: "shoe them away",
+          video: null,
+          image: "",
+          description: "",
+          effect: {
+            duration: 1,
+            algae_survive: 0.6,
+            growth_factor: 0.5
+          }
+        }
+      ]
     }
-  ],
-
-  
+  ]
 };
 
 module.exports = gameplay;
